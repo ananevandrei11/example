f1d233b7b7a53bdca02bbd41e06130ca796a3f13
Ananev Andrei <ananevandrei11@gmail.com>
2021-07-21 21:59:05 +0400

    add mail

--- phpless/send/send.php
+++ phpless/send/send.php
@@ -1,11 +1,31 @@
 <?php
 	if($_POST['captcha'] != 56) {
-		header('location: index.php');
+		header('location: /example/phpless/send');
 		exit;
 	}
 
 
-	if($_POST['subject'] == 'LESSON') {
-		$subject = 'LESSON';
+	if(!empty($_POST['subject'])) {
+		$subject = $_POST['subject'];
+	} else {
+		$subject = "FROM SITE";
 	}
-?>
+	
+	$to = $_POST['email'];
+	$from = "my_test_localhost@mail.ru";
+
+	$message = htmlspecialchars($_POST['msg']);
+	$message = urldecode($message);
+	$message = trim($message);
+
+	$headers = "From: $from" . "\r\n" .
+	"Reply-To: $from" . "\r\n" .
+	"X-Mailer: PHP/" . phpversion();
+
+	if(mail($to, $subject, $message, $headers)) {
+		header('location: /example/phpless/send');
+		exit;
+	} else {
+		echo 'Letter is not send';
+	}
+?> 
